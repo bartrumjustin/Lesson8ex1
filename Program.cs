4d64f125f51f7cee5e905dbf0f619b6b3d3310c8
bartrumjustin <bartrumjustin@gmail.com>
2026-03-03 10:43:14 -0500

    refactored for query method, original used lambda

--- Program.cs
+++ Program.cs
@@ -29,22 +29,37 @@
 
         static void Main(string[] args)
         {
-            var yearAfter1900 = stemPeople.Where(y => y.BirthYear > 1900);
-            Console.WriteLine("people who are born after 1900");
+            var yearAfter1900 = from y in stemPeople
+                                where y.BirthYear > 1900
+                                select y;
+            Console.WriteLine("people who are born after 1900\n");
             foreach (var entry in yearAfter1900)
             {
                 Console.WriteLine(entry.Name + " " + entry.BirthYear + "\n");
             }
-            var doubleLInName = stemPeople.Where(p => p.Name.Count(c => c == 'l') == 2);
+
+
+            var doubleLInName = from p in stemPeople
+                                where p.Name.Count(c => c == 'l') == 2
+                                select p;
             Console.WriteLine("names with 2 'l'");
             foreach (var entry in doubleLInName)
             {
                 Console.WriteLine(entry.Name + "\n");
             }
-            var After1950 = stemPeople.Count(y => y.BirthYear > 1950);
-            Console.WriteLine($"Number of people with a birthday after 1950: {After1950}" + "\n");
 
-            var entry1920to2000 = stemPeople.Where(d => d.BirthYear >= 1920 && d.BirthYear <= 2000);
+
+            var After1950 = from y in stemPeople
+                            where y.BirthYear > 1950
+                            select y;
+            int Count = After1950.Count();
+            Console.WriteLine($"Number of people with a birthday after 1950: {Count}" + "\n");
+
+
+
+            var entry1920to2000 = from d in stemPeople
+                                  where d.BirthYear >= 1920 && d.BirthYear <= 2000
+                                  select d;
             Console.WriteLine("People between 1920 - 2000");
             int count = 0;
             foreach (var entry in entry1920to2000)
@@ -53,15 +68,22 @@
                 Console.WriteLine($"{count}: " + entry.Name + "\n");
             }
             Console.WriteLine(count);
-            var sortBirthYear = stemPeople.OrderBy(y => y.BirthYear);
+
+
+            var sortBirthYear = from y in stemPeople
+                                orderby y.BirthYear ascending
+                                select y;
+            //stemPeople.OrderBy(y => y.BirthYear);
             Console.WriteLine("people sorted by birth year from unknown to latest: \n");
             foreach (var entry in sortBirthYear)
             {
                 Console.WriteLine(entry.Name + " " + entry.BirthYear + "\n");
             }
-            var deathYear = stemPeople
-                .Where(dY => dY.DeathYear > 1960 && dY.DeathYear < 2015)
-                .OrderBy(dY => dY.Name);
+
+
+            var deathYear = from dY in stemPeople
+                            where dY.DeathYear > 1960 && dY.DeathYear < 2015
+                            select dY;
             Console.WriteLine("Deaths > 1960 and < 2015 in ascending name order: \n");
             foreach (var death in deathYear)
             {
